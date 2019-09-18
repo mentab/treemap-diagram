4a4db9eea990daf1c4128ac9a99bcf450214f558
mentab <25363363+mentab@users.noreply.github.com>
2019-09-18 18:09:06 +0200

    add legend and text

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const w = 1200;
-const h = 1200;
+const w = 700;
+const h = 500;
 const m = 50;
 
 const content = d3.select('.content');
@@ -26,13 +26,13 @@ const legend = svg.append('g')
 
 d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json')
 	.then(data => {
-		const colorScale = d3.scaleOrdinal(d3.schemeAccent)
+		const colorScale = d3.scaleOrdinal(['#FFFF00', '#FA8072', '#7FFF00', '#008080', '#00FA9A', '#EE82EE', '#6495ED', '#FFE4E1', '#FFDAB9', '#FF4500', '#E9967A', '#40E0D0', '#6B8E23', '#FFA07A', '#FFFAFA', '#C0C0C0', '#FF1493', '#808080', '#8A2BE2', '#CD853F' ])
 			.domain(data.children.map(children => children.name));
 
 		const root = d3.hierarchy(data).sum(d => d.value);
 
 		d3.treemap()
-			.size([1000, 1000])
+			.size([w, h])
 			(root);
 
 		svg.selectAll('rect')
@@ -51,31 +51,44 @@ d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/
 			.attr('fill', d => colorScale(d.parent.data.name))
 			.on('mouseover', d => {
 				tooltip.attr('data-value', d.data.value)
-					.style('left', 0 + 'px')
-					.style('top', 0 + 'px')
+					.style('left', d3.event.pageX + 15 + 'px')
+					.style('top', d3.event.pageY - 25 + 'px')
 					.style('opacity', .9)
-					.html(() => `<p></p>`);
+					.html(() => `<p><strong>${d.data.name}</strong>: ${d.data.value}</p>`);
 			})
 			.on('mouseout', () => {
 				tooltip.style('opacity', 0)
 					.html(() => '');
 			});;
 
-		legend.selectAll('circle')
+		svg.selectAll('text')
+			.data(root.leaves())
+			.enter()
+			.append('text')
+			.attr('x', d => d.x0)
+			.attr('y', d => d.y0)
+			.attr('class', 'tile-text')
+			.attr('fill', 'black')
+			.attr('text-anchor', 'start')
+			.text(d => d.data.name);
+
+		legend.selectAll('rect')
 			.data(data.children)
 			.enter()
-			.append('circle')
-			.attr('cx',0)
-			.attr('cy', 0)
-			.attr('r', 5)
+			.append('rect')
+			.attr('x', (d, i) => i % 3 * 100 + 300)
+			.attr('y', (d, i) => Math.ceil((i + 1) / 3) * 30 + 995)
+			.attr('width', 10)
+			.attr('height', 10)
+			.attr('class', 'legend-item')
 			.attr('fill', d => colorScale(d.name));
 
 		legend.selectAll('text')
 			.data(data.children)
 			.enter()
 			.append('text')
-			.attr('x', 0)
-			.attr('y', 0)
+			.attr('x', (d, i) => i % 3 * 100 + 315)
+			.attr('y', (d, i) => Math.ceil((i + 1) / 3) * 30 + 1000)
 			.style('alignment-baseline', 'middle')
 			.text(d => d.name);
 	});
