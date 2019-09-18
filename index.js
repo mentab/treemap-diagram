b3de069da28141c2aa9b0b76ab7480a7642f6ebb
mentab <25363363+mentab@users.noreply.github.com>
2019-09-18 13:31:50 +0200

    continue project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const w = 1200;
-const h = 600;
+const h = 1200;
 const m = 50;
 
 const content = d3.select('.content');
@@ -21,8 +21,14 @@ const svg = content.append('svg')
 	.attr('width', w)
 	.attr('height', h);
 
+const legend = svg.append('g')
+	.attr('id', 'legend');
+
 d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/video-game-sales-data.json')
 	.then(data => {
+		const colorScale = d3.scaleOrdinal(d3.schemeAccent)
+			.domain(data.children.map(children => children.name));
+
 		const root = d3.hierarchy(data).sum(d => d.value);
 
 		d3.treemap()
@@ -37,13 +43,14 @@ d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/
 			.attr('y', d => d.y0)
 			.attr('width', d => d.x1 - d.x0)
 			.attr('height', d => d.y1 - d.y0)
-			.attr('class', 'that represent the data')
-			.attr('data-name', 'that represent the data')
-			.attr('data-category', 'that represent the data')
-			.attr('data-value', 'that represent the data')
-			.attr('fill', 'black')
+			.attr('class', 'tile')
+			.attr('data-name', d => d.data.name)
+			.attr('data-category', d => d.data.category)
+			.attr('data-value', d => d.data.value)
+			.attr('stroke', 'white')
+			.attr('fill', d => colorScale(d.parent.data.name))
 			.on('mouseover', d => {
-				tooltip.attr('data-toto', 'toto')
+				tooltip.attr('data-value', d.data.value)
 					.style('left', 0 + 'px')
 					.style('top', 0 + 'px')
 					.style('opacity', .9)
@@ -53,4 +60,22 @@ d3.json('https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/
 				tooltip.style('opacity', 0)
 					.html(() => '');
 			});;
+
+		legend.selectAll('circle')
+			.data(data.children)
+			.enter()
+			.append('circle')
+			.attr('cx',0)
+			.attr('cy', 0)
+			.attr('r', 5)
+			.attr('fill', d => colorScale(d.name));
+
+		legend.selectAll('text')
+			.data(data.children)
+			.enter()
+			.append('text')
+			.attr('x', 0)
+			.attr('y', 0)
+			.style('alignment-baseline', 'middle')
+			.text(d => d.name);
 	});
